39c150f02eca040d8c1404121ebfc7a6ded6061e
daibing <daibing125@foxmail.com>
2018-09-06 14:34:59 +0800

    基于ssm框架的增删改查项目,页面介于bootstrap

---
+++ src/main/java/com/daibing/crud/controller/DepartmentController.java
@@ -0,0 +1,35 @@
+package com.daibing.crud.controller;
+
+import com.daibing.crud.bean.Department;
+import com.daibing.crud.bean.Msg;
+import com.daibing.crud.service.DepartmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+/**
+ * 处理和部门有关的请求
+ * @author lfy
+ *
+ */
+@Controller
+public class DepartmentController {
+	
+	@Autowired
+	private DepartmentService departmentService;
+	
+	/**
+	 * 返回所有的部门信息
+	 */
+	@RequestMapping("/depts")
+	@ResponseBody
+	public Msg getDepts(){
+		//查出的所有部门信息
+		List<Department> list = departmentService.getDepts();
+		return Msg.success().add("depts", list);
+	}
+
+}
---
+++ src/main/java/com/daibing/crud/service/DepartmentService.java
@@ -0,0 +1,22 @@
+package com.daibing.crud.service;
+
+import com.daibing.crud.bean.Department;
+import com.daibing.crud.dao.DepartmentMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DepartmentService {
+	
+	@Autowired
+	private DepartmentMapper departmentMapper;
+
+	public List<Department> getDepts() {
+		// TODO Auto-generated method stub
+		List<Department> list = departmentMapper.selectByExample(null);
+		return list;
+	}
+
+}
